8fd36c94e1de8041f541ed3301399a38e615acc6
William <wtbell4080@gmail.com>
2021-02-23 13:32:35 -0500

    Add regular expressions to test inputs lines 22-29. Re-did function that checks each field on button click lines 40-80

--- javascript/reservations.js
+++ javascript/reservations.js
@@ -8,32 +8,81 @@ const yearInput = document.getElementById('year-input');
 const hourInput = document.getElementById('hour-input');
 const minuteInput = document.getElementById('minute-input');
 const amPmInput = document.getElementById('am-pm-input');
-const personInput = document.getElementById('person-input');
+const peopleInput = document.getElementById('people-input');
 
-/* Variables for error message inputs */
+/* Variables for error message outputs */
 const nameError = document.getElementById('error-message-name');
 const emailError = document.getElementById('error-message-email');
 const dateError = document.getElementById('error-message-date');
 const timeError = document.getElementById('error-message-time');
 const peopleError = document.getElementById('error-message-people');
 
-/* Add event listener on form button to check input */
-document.getElementById('form-button').addEventListener('click', checkInput);
 
-/* Check input fields and display error message if a field is missing */
-function checkInput(e){
-  if(nameInput.value == ""){
-    nameError.textContent = "This field is required";
-  }else if(emailInput.value == ""){
-    emailError.textContent = "This field is required";
-  }else if(monthInput.value == "" || dayInput.value == "" || yearInput.value == ""){
-    dateError.textContent = "This field is incomplete";
-  }else if(hourInput.value == "" || minuteInput.value == "" || amPmInput.value == ""){
-    timeError.textContent = "This field is incomplete";
-  }else if(personInput.value == ""){
-    personError.textContent = "This field is required";
+
+/* Regular expressions to test against specific form inputs */
+ const nameRegEx = /^[a-zA-Z]{1,50}$/;
+ const dayMonthRegEx = /^[0-9]{1,2}$/;
+ const yearRegEx = /^[0-9]{4}$/;
+ const hourRegEx = /^[0-9]{1,2}$/;
+ const minutesRegEx = /^[0-9]{2}$/;
+ const peopleRegEx = /^[0-9]{1,2}$/;
+ const emailRegEx = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+ 
+
+
+/* Event listener on form button to validate input (validateFields function) */
+document.getElementById('form-button').addEventListener('click', validateFields);
+
+
+
+
+/* Validate fields function that checks field values on button click*/
+function validateFields(e) {
+  // If name is empty or doesn't match expression, show error
+  if(nameInput.value == "" || !nameInput.value.match(nameRegEx)){
+    nameError.textContent ="Invalid -needs 10 letters";
+  } else if(nameInput.value.match(nameRegEx)){
+    nameError.textContent ="";
   }
-  
+
+  // If email is empty or doesn't match expression, show error
+  if(emailInput.value == "" || !emailInput.value.match(emailRegEx)){
+    emailError.textContent ="Invalid Email";
+  } else if(emailInput.value.match(emailRegEx)){
+    emailError.textContent ="";
+  }
+
+  // If day, month, or year are empty, or don't match expression, show error
+  if(dayInput.value == "" || !dayInput.value.match(dayMonthRegEx) || 
+  monthInput.value == "" || !monthInput.value.match(dayMonthRegEx)||
+  yearInput.value == "" || !yearInput.value.match(yearRegEx)){
+    dateError.textContent = "Invalid Field";
+  }else if(dayInput.value.match(dayMonthRegEx) && monthInput.value.match(dayMonthRegEx) && yearInput.value.match(yearRegEx)){
+    dateError.textContent = "";
+  }
+
+  // If hour or minutes are empty, or don't match expression, show error
+  if(hourInput.value == "" || !hourInput.value.match(hourRegEx) || 
+  minuteInput.value == "" || !minuteInput.value.match(minutesRegEx)){
+    timeError.textContent = "Invalid Field";
+  }else if(minuteInput.value.match(minutesRegEx) && hourInput.value.match(hourRegEx)){
+    timeError.textContent = "";
+  }
+
+  // If people input is empty or doesn't match expression, show error
+  if(peopleInput.value == "" || !peopleInput.value.match(peopleRegEx)){
+    peopleError.textContent = "Invalid Field";
+  }else if(peopleInput.value.match(peopleRegEx)){
+    peopleError.textContent = "";
+  }
+
   e.preventDefault();
 }
+
+
+
+
+
+
+
 
