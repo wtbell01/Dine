287dbe5b84c2ab92ce3eb3ad4d2e9bacd680068d
William <wtbell4080@gmail.com>
2021-02-25 11:20:10 -0500

    Event listeners to change book link colors lines 11,16,20. Functions to chang booking link colors lines 24,33,42

--- javascript/index.js
+++ javascript/index.js
@@ -1,22 +1,51 @@
 /* Declare variables for booking links and booking outputs */
-const bookingFamilyLink = document.getElementById('booking-family-link');
-const bookingSocialLink = document.getElementById('booking-social-link');
-const bookingSpecialLink = document.getElementById('booking-special-link');
+const bookingFamilyLink = document.querySelector('.booking-family-link');
+const bookingSocialLink = document.querySelector('.booking-social-link');
+const bookingSpecialLink = document.querySelector('.booking-special-link');
 const bookingImage = document.getElementById('booking-image');
 const bookingHeader = document.getElementById('booking-header');
 const bookingDescription = document.getElementById('booking-description-text');
 
-/* Event listener for the family link that outputs family info when function is ran  */
-document.querySelector('.booking-family-link').addEventListener('click', displayFamily);
-
-/* Event listener for the social link that outputs social info when function is ran */
-document.querySelector('.booking-social-link').addEventListener('click', displaySocial);
-
-/* Event listener for the special link that outputs special info when function is ran */
-document.querySelector('.booking-special-link').addEventListener('click', displaySpecial);
+/* Event listeners for the family link  */
+bookingFamilyLink.addEventListener('click', displayFamily);
+bookingFamilyLink.addEventListener('click', displayFamilyLink);
 
 
+/* Event listeners for the social link  */
+bookingSocialLink.addEventListener('click', displaySocial);
+bookingSocialLink.addEventListener('click', displaySocialLink);
 
+/* Event listeners for the special link  */
+bookingSpecialLink.addEventListener('click', displaySpecial);
+bookingSpecialLink.addEventListener('click', displaySpecialLink);
+
+
+/* Add active class (dark link color) to family link and remove active class from the rest */
+function displayFamilyLink(e){
+  bookingFamilyLink.classList.add('booking-active-link');
+  bookingSocialLink.classList.remove('booking-active-link');
+  bookingSpecialLink.classList.remove('booking-active-link');
+  e.preventDefault();
+}
+
+
+/* Add active class (dark link color) to social link and remove active class from the rest */
+function displaySocialLink(e){
+  bookingSocialLink.classList.add('booking-active-link');
+  bookingSpecialLink.classList.remove('booking-active-link');
+  bookingFamilyLink.classList.remove('booking-active-link');
+  e.preventDefault();
+}
+
+
+/* Add active class (dark link color) to social link and remove active class from the rest */
+function displaySpecialLink(e){
+  bookingSpecialLink.classList.add('booking-active-link');
+  bookingSocialLink.classList.remove('booking-active-link');
+  bookingFamilyLink.classList.remove('booking-active-link');
+  
+  e.preventDefault();
+}
 
 /* Function that retrievs the social json file and loops through the object and displays it to the booking output */
 function displayFamily(e){
